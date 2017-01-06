bf8d7f46ddb703fe27e27c4f49c3845a87e257fd
Patrick S. Watters <pwatters@ups.com>
2017-01-06 17:39:37 -0500

    csv file & final random forest method

--- introML/algo_scratch/random_forest.py
+++ introML/algo_scratch/random_forest.py
@@ -101,7 +101,7 @@ def gini_index(groups, class_values):
 			gini += (proportion * (1.0 - proportion))
 	return gini
 
-
+# split the best split for a dataset
 def get_split(dataset, n_features):
 	class_values = list(set(row[-1] for row in dataset))
 	b_index, b_value, b_score, b_groups = 999, 999, 999, None
@@ -121,10 +121,82 @@ def get_split(dataset, n_features):
 				b_value  = row[index]
 				b_score  = gini
 				b_groups = groups
+
 	return {'index':b_index, 'value':b_value, 'groups':b_groups}
 
+#create a terminal node value
+def to_terminal(group):
+	outcomes = [row[-1] for row in group]
+	return max(set(outcomes), key = outcomes.count)
 
+# create child splits for a node or to make terminal
+def split(node, max_depth, min_size, n_features, depth):
+	left, right = node['groups']
+	del(node['groups'])
 
+	# check for a no split
+	if not left or not right:
+		node['left'] = node['right'] = to_terminal(left + right)
+		return
+	# check for max depth
+	if depth >= max_depth:
+		node['left'], node['right'] = to_terminal(left), to_terminal(right)
+		return
+	# process left child
+	if len(left) <= min_size:
+		node['left'] = to_terminal(left)
+	else:
+		node['left'] = get_split(left, n_features)
+		split(node['left'], max_depth, min_size, n_features, depth+1)
+	# process right child
+	if len(right) <= min_size:
+		node['right'] = to_terminal(right)
+	else:
+		node['right'] = get_split(right, n_features)
+		split(node['right'], max_depth, min_size, n_features, depth+1)
+
+# Build a decision tree
+def build_tree(train, max_depth, min_size, n_features):
+	root = get_split(dataset, n_features)
+	split(root, max_depth, min_size, n_features, 1)
+	return root
+ 
+# Make a prediction with a decision tree
+def predict(node, row):
+	if row[node['index']] < node['value']:
+		if isinstance(node['left'], dict):
+			return predict(node['left'], row)
+		else:
+			return node['left']
+	else:
+		if isinstance(node['right'], dict):
+			return predict(node['right'], row)
+		else:
+			return node['right']
+ 
+# Create a random subsample from the dataset with replacement
+def subsample(dataset, ratio):
+	sample = list()
+	n_sample = round(len(dataset) * ratio)
+	while len(sample) < n_sample:
+		index = randrange(len(dataset))
+		sample.append(dataset[index])
+	return sample
+ 
+# Make a prediction with a list of bagged trees
+def bagging_predict(trees, row):
+	predictions = [predict(tree, row) for tree in trees]
+	return max(set(predictions), key=predictions.count)
+
+# Random Forest Algorithm
+def random_forest(train, test, max_depth, min_size, sample_size, n_trees, n_features):
+	trees = list()
+	for i in range(n_trees):
+		sample = subsample(train, sample_size)
+		tree = build_tree(sample, max_depth, min_size, n_features)
+		trees.append(tree)
+	predictions = [bagging_predict(trees, row) for row in test]
+	return(predictions)
 
 
 # Test the random forest algorithm
