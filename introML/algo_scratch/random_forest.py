fa8c76e8ed335add0ecd26315040cbc526ac51de
Patrick S. Watters <pwatters@ups.com>
2017-01-06 17:23:30 -0500

    methods to score the algorithm

--- introML/algo_scratch/random_forest.py
+++ introML/algo_scratch/random_forest.py
@@ -47,6 +47,36 @@ def cross_validation_split(dataset, n_folds):
 		dataset_split.append(fold)
 	return dataset_split
 
+def accuracy_metric(actual, predicted):
+	correct = 0
+
+	for i in range(len(actual)):
+		if actual[i] == predicted[i]:
+			correct += 1
+	return (correct / float(len(actual)) * 100.0)
+
+# evaluate an algorithm using a cross validation split
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+	folds = cross_validation_split(dataset, n_folds)
+	scores = list()
+
+	for fold in folds:
+		train_set = list(folds)
+		train_set.remove(fold)
+		train_set = sum(train_set, [])
+		test_set = list()
+
+		for row in fold:
+			row_copy = list(row)
+			test_set.append(row_copy)
+			row_copy[-1] = None
+
+		predicted = algorithm(train_set, test_set, *args)
+		actual = [row[-1] for row in fold]
+		accuracy = accuracy_metric(actual, predicted)
+		scores.append(accuracy)
+	return scores
+
 def get_split(dataset, n_features):
 	class_values = list(set(row[-1] for row in dataset))
 	b_index, b_value, b_score, b_groups = 999, 999, 999, None
