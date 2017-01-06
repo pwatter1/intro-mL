4d66c95aa0626899a0b42273806527215dcd006f
Patrick S. Watters <pwatters@ups.com>
2017-01-06 17:12:47 -0500

    formatting data & cross_validation_split

--- introML/algo_scratch/random_forest.py
+++ introML/algo_scratch/random_forest.py
@@ -1,6 +1,51 @@
+# Random Forest Algorithm on Sonar Dataset
+# MachineLearningMastery.com RF Algo tutorial.
 
+from random import seed
+from random import randrange
+from csv import reader
+from math import sqrt
+ 
+# Load a CSV file
+def load_csv(filename):
+	dataset = list()
+	with open(filename, 'r') as file:
+		csv_reader = reader(file)
+		for row in csv_reader:
+			if not row:
+				continue
+			dataset.append(row)
+	return dataset
 
-# MachineLearningMastery.com Random Forest Algo tutorial.
+def str_column_to_float(dataset, column):
+	for row in dataset:
+		row[column] = float(row[column].strip())
+
+def str_column_to_int(dataset, column):
+	class_values = [row[column] for row in dataset]
+	unique = set(class_values)
+	lookup = dict()
+
+	for i, value in enumerate(unique):
+		lookup[value] = i
+
+	for row in dataset:
+		row[column] = lookup[row[column]]
+	return dataset
+
+# split dataset into k folds
+def cross_validation_split(dataset, n_folds):
+	dataset_split = list()
+	dataset_copy = list(dataset)
+	fold_size = int(len(dataset) / n_folds)
+
+	for i in range(n_folds):
+		fold = list()
+		while len(fold) < fold_size:
+			index = randrange(len(dataset_copy))
+			fold.append(dataset_copy.pop(index))
+		dataset_split.append(fold)
+	return dataset_split
 
 def get_split(dataset, n_features):
 	class_values = list(set(row[-1] for row in dataset))
@@ -17,6 +62,8 @@ def get_split(dataset, n_features):
 			groups = test_split(index, row[index], dataset)
 			gini = gini_index(groups, class_values)
 			if gini < b_score:
-				b_index, b_value, b_score, b_groups = index, row[index], gini, groups
-	
+				b_index  = index
+				b_value  = row[index]
+				b_score  = gini
+				b_groups = groups
 	return {'index':b_index, 'value':b_value, 'groups':b_groups}
