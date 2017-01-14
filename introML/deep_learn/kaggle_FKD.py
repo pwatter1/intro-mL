fba407199f8012828546d0f84005791528929a2e
Patrick S. Watters <pwatters@ups.com>
2017-01-13 21:08:38 -0500

    refactoring load() and first neural net

--- introML/deep_learn/kaggle_FKD.py
+++ introML/deep_learn/kaggle_FKD.py
@@ -1,73 +1,85 @@
+# Kaggle FKD Tutorial
+
+# rid deprecation warnings
+def warn(*args, **kwargs):
+    pass
+
 import os
 import numpy as np
+import warnings
+warnings.warn = warn
 
 from pandas import read_csv
-from sklearn.utils import shuffle, validation
+from sklearn.utils import shuffle
 from lasagne import layers
 from lasagne.updates import nesterov_momentum
 from nolearn.lasagne import NeuralNet
 
+FTEST  = '~/Documents/introML/introML/deep_learn/test.csv'
 FTRAIN = '~/Documents/introML/introML/deep_learn/training.csv'
-FTEST = '~/Documents/introML/introML/deep_learn/training.csv'
 
 def load(test=False, cols=None):
-    ''' 
-    loads data from FTEST if test is true, otherwise from FTRAIN.
-    pass a list of cols if interested in subset of target columns. 
-    '''
+    # Loads data from FTEST if file present, otherwise from FTRAIN.
+    # Pass a list of cols if you're only interested in a subset of the
+    # target columns.
     
     file_name = FTEST if test else FTRAIN
-    df = read_csv(os.path.expanduser(file_name)) # load pandas dataframe
+    # load pandas dataframe
+    df = read_csv(os.path.expanduser(file_name))  
 
-    # The Image column has pixel values separated by space; convert
-    # the values to numpy arrays:
+    # image column has pixel values separated by space; convert to numpy arrays
     df['Image'] = df['Image'].apply(lambda im: np.fromstring(im, sep=' '))
 
-    if cols:  # get a subset of columns
+    # get a subset of columns
+    if cols:  
         df = df[list(cols) + ['Image']]
 
-    print(df.count())  # prints the number of values for each column
-    df = df.dropna()  # drop all rows that have missing values in them
+    print(df.count()) 
+    df =  df.dropna()   # drop all rows that have missing values in them
 
-    X = np.vstack(df['Image'].values) / 255.  # scale pixel values to [0, 1]
+    # scale pixel values to [0, 1] as opposed to [0, 255]
+    X = np.vstack(df['Image'].values) / 255.0  
     X = X.astype(np.float32)
 
     if not test:  # only FTRAIN has any target columns
-        Y = df[df.columns[:-1]].values
-        Y = (Y - 48) / 48  # scale target coordinates to [-1, 1]
-        X = shuffle(X, Y, random_state=42)  # shuffle train data
-        Y = shuffle(X, Y, random_state=42) 
-        Y = Y.astype(np.float32)
+        y = df[df.columns[:-1]].values
+        y = (y - 48) / 48  # scale target coordinates to [-1, 1]
+        X, y = shuffle(X, y, random_state=42)
+        y = y.astype(np.float32)
     else:
-        Y = None
+        y = None
 
-    return X, Y
+    return X, y
 
-'''
-X, Y = load()
+
+X, y = load()
 print("X.shape == {}; X.min == {:.3f}; X.max == {:.3f}".format(
     X.shape, X.min(), X.max()))
 print("y.shape == {}; y.min == {:.3f}; y.max == {:.3f}".format(
-    Y.shape, Y.min(), Y.max()))
-'''
+    y.shape, y.min(), y.max()))
 
-net1 = NeuralNet(layers = [('input', layers.InputLayer),
-                           ('hidden', layers.DenseLayer),
-                           ('output', layers.DenseLayer)],
-                           # layer parameters:
-                           input_shaper = (None, 9216), # 96x96 pixels per batch
-                           hidden_num_units = 100, # number of units in hidden layer
-                           output_nonlinearity = None, 
-                           output_num_units = 30, # 30 target values
+net1 = NeuralNet(layers=[  
+                        ('input', layers.InputLayer),
+                        ('hidden', layers.DenseLayer),
+                        ('output', layers.DenseLayer),
+                        ],
 
-                           # optimization method:
-                           update = nesterov_momentum,
-                           update_learning_rate = 0.1,
-                           update_momentum = 0.9,
+    # layer parameters:
+    input_shape=(None, 9216),  # 96x96 input pixels per batch
+    hidden_num_units=100,      # number of units in hidden layer
+    output_nonlinearity=None,  # output layer uses identity function
+    output_num_units=30,       # 30 target values
 
-                           regression = True, # indicate regression problem
-                           max_epochs = 300, 
-                           verbose = 1,)
+    # optimization method:
+    update=nesterov_momentum,
+    update_learning_rate=0.01,
+    update_momentum=0.9,
 
-X, Y = load()
-net1.fit(X, Y)
+    regression=True,  # flag to indicate we're dealing with regression problem
+    max_epochs=400,   
+    verbose=1,
+    )
+
+X, y = load()
+net1.fit(X, y)
+
