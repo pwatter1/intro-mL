ff94082c80292a5bbcad91c1ac32066fe3fffe7b
Patrick S. Watters <pwatters@ups.com>
2017-01-13 21:43:31 -0500

    plotting predictions against sample faces

--- introML/deep_learn/kaggle_FKD.py
+++ introML/deep_learn/kaggle_FKD.py
@@ -6,6 +6,7 @@ def warn(*args, **kwargs):
 
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 import warnings
 warnings.warn = warn
 
@@ -14,6 +15,7 @@ from sklearn.utils import shuffle
 from lasagne import layers
 from lasagne.updates import nesterov_momentum
 from nolearn.lasagne import NeuralNet
+
 
 FTEST  = '~/Documents/introML/introML/deep_learn/test.csv'
 FTRAIN = '~/Documents/introML/introML/deep_learn/training.csv'
@@ -35,7 +37,8 @@ def load(test=False, cols=None):
         df = df[list(cols) + ['Image']]
 
     print(df.count()) 
-    df =  df.dropna()   # drop all rows that have missing values in them
+    # drop all rows twith missing values 
+    df =  df.dropna()   
 
     # scale pixel values to [0, 1] as opposed to [0, 255]
     X = np.vstack(df['Image'].values) / 255.0  
@@ -51,13 +54,15 @@ def load(test=False, cols=None):
 
     return X, y
 
-
-X, y = load()
+# describe data 
+'''
 print("X.shape == {}; X.min == {:.3f}; X.max == {:.3f}".format(
     X.shape, X.min(), X.max()))
 print("y.shape == {}; y.min == {:.3f}; y.max == {:.3f}".format(
     y.shape, y.min(), y.max()))
+'''
 
+# first model
 net1 = NeuralNet(layers=[  
                         ('input', layers.InputLayer),
                         ('hidden', layers.DenseLayer),
@@ -66,8 +71,8 @@ net1 = NeuralNet(layers=[
 
     # layer parameters:
     input_shape=(None, 9216),  # 96x96 input pixels per batch
-    hidden_num_units=100,      # number of units in hidden layer
-    output_nonlinearity=None,  # output layer uses identity function
+    hidden_num_units=100,      # number of nodes in hidden layer
+    output_nonlinearity=None,  # output layer uses identity function?
     output_num_units=30,       # 30 target values
 
     # optimization method:
@@ -75,11 +80,28 @@ net1 = NeuralNet(layers=[
     update_learning_rate=0.01,
     update_momentum=0.9,
 
-    regression=True,  # flag to indicate we're dealing with regression problem
-    max_epochs=400,   
+    regression=True,  # flag indicates regression problem, not classification
+    max_epochs=100,   # number of trials
     verbose=1,
     )
 
 X, y = load()
 net1.fit(X, y)
 
+def plot_sample(x, y, axis):
+    img = x.reshape(96, 96)
+    axis.imshow(img, cmap='gray')
+    axis.scatter(y[0::2] * 48 + 48, y[1::2] * 48 + 48, marker='x', s=10)
+
+X, _ = load(test=True)
+y_pred = net1.predict(X)
+
+fig = plt.figure(figsize=(6, 6))
+fig.subplots_adjust(
+    left=0, right=1, bottom=0, top=1, hspace=0.05, wspace=0.05)
+
+for i in range(16):
+    ax = fig.add_subplot(4, 4, i + 1, xticks=[], yticks=[])
+    plot_sample(X[i], y_pred[i], ax)
+
+plt.show()
